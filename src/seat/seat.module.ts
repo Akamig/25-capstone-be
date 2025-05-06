b3af951efe54c0dc24906d0ef2bcf3488c95bbd8
Akamig <akamig@posteo.net>
2025-05-06 19:44:48 +0900

    Add WebSocket Gateway

--- src/seat/seat.module.ts
+++ src/seat/seat.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SeatController } from './seat.controller';
 import { SeatService } from './seat.service';
+import { SeatStatusGateway } from './seat-status/seat-status.gateway';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [SeatController],
-  providers: [SeatService],
+  providers: [SeatService, SeatStatusGateway],
 })
 export class SeatModule {}
